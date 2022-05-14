eb84ac8ab40c20df3be27137a47a8e1aa4c6ef30
Hirohisa Kitsuka <hi.kitsuka@gmail.com>
2022-05-14 23:58:48 +0900

    ⚡

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# カレントディレクトリに go.mod を作成
+## カレントディレクトリに go.mod を作成
 
 > go mod init go_basic
 
-# go.mod の中身
+## go.mod の中身
 
 > cat go.mod
 
 module go_basic と記述されているだけ
+
+## 標準ライブラリのドキュメント
+
+https://pkg.go.dev/std
+
+## godoc コマンドのインストール
+
+> go get golang.org/x/tools/cmd/godoc
--- lesson.go
+++ lesson.go
@@ -1,6 +1,25 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"os/user"
+	"time"
+)
+
+
+// 一番初めに実行される特別な関数
+// パッケージの初期化に用いられる
+// main 関数よりも先に呼ばれる
+func init() {
+	fmt.Println("Init!")
+}
+
+// 関数の書き方
+func bazz() {
+	fmt.Println("Bazz")
+}
 
 func main(){
-	fmt.Println("Hello world!")
+	bazz()
+	fmt.Println("Hello","World",time.Now())
+	fmt.Println(user.Current())
 }
