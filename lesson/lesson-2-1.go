1751e8109c1dd6520c66b30445c484e6d21a3e4c
kits-create <hi.kitsuka@gmail.com>
2022-06-09 12:56:23 +0900

    🏌️

--- lesson/lesson-2-1.go
+++ lesson/lesson-2-1.go
@@ -1,22 +1,42 @@
-package lesson
+package main
 
+import (
+	"fmt"
+	"math"
+)
+
+// Convert Decimal to Binary
 func convDecToBin(val int) []int {
 	var result []int
 	num := val
 	for {
 		quotient := num / 2
 		mod := num % 2
-
 		result = append([]int{mod}, result[0:]...)
 
 		num = quotient
 		if quotient == 0 || quotient == 1 {
+			result = append([]int{quotient}, result[0:]...)
 			break
 		}
 	}
 	return result
 }
 
+// Convert Binary to Decimal
+func convBinToDec(val []int) int {
+	var sum int
+	for i := 0; i < len(val) -1; i++ {
+		sum += int(math.Pow(2,float64(len(val) - 1 - i)) * float64(val[i]))
+	}
+	return sum
+}
+
+
 func main() {
-	convDecToBin(100)
+	res := convDecToBin(100)
+	fmt.Printf("%d\n",res)
+
+	res2 := convBinToDec(res)
+	fmt.Printf("%d\n",res2)
 }
